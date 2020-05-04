fa452f982ba71bd615236b438633e017bfac77d9
Vasil Arabadzhiev <VA404@localhost.localdomain>
2020-05-04 10:53:11 +0300

    added styles

--- sseorders-web/src/App.js
+++ sseorders-web/src/App.js
@@ -1,29 +1,33 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import Contractor from './Contractor'
+import Contractor from './Contractor';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Navbar, Nav} from "react-bootstrap";
 
 class App extends Component {
 
     render() {
         return (
             <Router>
-                <div>
-                <ul>
-                    <li>
-                        <Link to="/alice">Alice</Link>
-                    </li>
-                    <li>
-                        <Link to="/bob">Bob</Link>
-                    </li>
-                </ul>
-                <Switch>
-                    <Route path="/alice">
-                        <Contractor name="alice"/>
-                    </Route>
-                    <Route path="/bob">
-                        <Contractor name="bob"/>
-                    </Route>
-                </Switch>
+                <div className="text-center">
+                    <Navbar bg="dark" variant="dark">
+                        <Nav className="m-auto">
+                            <Nav.Item>
+                                <Link  to="/alice">Alice</Link>
+                            </Nav.Item>
+                            <Nav.Item  className="ml-3" >
+                                <Link to="/bob">Bob</Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Navbar>
+                    <Switch>
+                        <Route path="/alice">
+                            <Contractor name="alice"/>
+                        </Route>
+                        <Route path="/bob">
+                            <Contractor name="bob"/>
+                        </Route>
+                    </Switch>
                 </div>
             </Router>
         )
